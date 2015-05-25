924b960d021327c6ee937365e99a323f54ac09fb
Kennyomg <kenrick_half8@hotmail.com>
2015-05-25 12:29:38 +0200

    FPS counter addad

--- SFML-playground/SFML-playground/src/main.cpp
+++ SFML-playground/SFML-playground/src/main.cpp
@@ -1,13 +1,19 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include<iostream>
+#include "FPS.h"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
-
+	window.setFramerateLimit(30);
+	FPS fps;
+	
 	while (window.isOpen())
 	{
+		
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -15,9 +21,12 @@ int main()
 				window.close();
 		}
 
+		shape.setPosition(sf::Vector2f(shape.getPosition().x + 1.f, shape.getPosition().y));
 		window.clear();
 		window.draw(shape);
 		window.display();
+		fps.update();
+		std::cout << "fps: " << fps.getFPS() << std::endl;
 	}
 
 	return 0;
